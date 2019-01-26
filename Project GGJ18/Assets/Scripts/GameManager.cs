e5f371300c5dd257828ca5e75cda525c2ba5ca99
Gonzalo <escamillagonzalo@gmail.com>
2019-01-26 03:15:49 -0300

    Try

--- Project GGJ18/Assets/Scripts/GameManager.cs
+++ Project GGJ18/Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+/*
     private static GameManager _instance;
     public static GameManager instance {
         get {
@@ -13,8 +15,29 @@ public class GameManager : MonoBehaviour {
             return _instance;
         }
     }
+*/
+
+   public GameObject GOPlayer;
+   public PlayerManager _PlayerManager;
 
 
+    // light: valor de la cantidad de luz que tiene el personaje disponible, varia entre 0 y 30 y disminuye linealmente en una unidad por segundo.
+    // flame: valor de la cantidad de tiempo que nos queda de llama en el hogar. varia entre 0 y 60 y disminuye afectado por un coeficiente
+    //        que varia segund el tiempo de partida
+
+    [Range(0f,60f)]
+    public float light;
+    [Range(0f,30f)]
+    public float flame;
+    // Tiempo de espera para actualizar los estados y valores del juego: Light, Flame, etc.
+    [Range (0.1f, 0.5f)]
+    public float waitTime;
+
+    // orb: El contador de orbes.
+    // time: contador del tiempo en segudos, va a ser el contador total de tiempo que te mantenes vivo.
+    int orbs, time;
+
+    public Text txtFlame,txtLight,txtOrbs,txtTime;
 
     void Awake () {
 		
@@ -22,9 +45,54 @@ public class GameManager : MonoBehaviour {
 	
 	void Start () {
 		
+		Initialize();
+		StartCoroutine("ChangeStates");
 	}
 	
 	void Update () {
 		
+		TextUpdate();
 	}
+
+	void Initialize (){
+
+		_PlayerManager = GameObject.FindObjectOfType<PlayerManager>();
+		//_PlayerManager = GOPlayer.gameObject.GetComponent<PlayerManager>();
+
+
+		// Establezco el valor inicial de las variables.
+		light    = 30;
+		flame    = 60;
+		orbs     = 0;
+		time     = 0;
+		waitTime = 0.1f;
+	}
+
+	void TextUpdate(){
+
+		// Para debugear
+		txtTime.text  = "Time: "  + time.ToString();
+		txtOrbs.text  = "Orbs: "  + orbs.ToString();
+		txtLight.text = "Light: " + light.ToString();
+		txtFlame.text = "Flame: " + flame.ToString();
+	}
+
+	IEnumerator ChangeStates(){
+
+		while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            //time = (INT)Time.deltaTime;
+            flame -= 1 * waitTime; 
+            if(light >= 0 && light <= 30){
+
+            	  light -= 1 * waitTime * _PlayerManager.lightChangeFactor;
+            	  	if(light > 30)
+            	  		light = 30;
+
+            }
+           
+        }
+	}
+
 }
--- Project GGJ18/Assets/Scripts/PlayerManager.cs
+++ Project GGJ18/Assets/Scripts/PlayerManager.cs
@@ -6,6 +6,10 @@ public class PlayerManager : MonoBehaviour {
 
     public Rigidbody rBody;
     public float speed; //4
+    // isHome muestra si el pj esta dentro de la cueva.
+    public bool isHome;
+    // Basicamente determina la velocidad en la que cambia la subida o bajada de la variable de luz, si esta en casa sube rapido, sino baja normalmente. 
+    public int lightChangeFactor;
 
     void Awake () {
 		
@@ -13,6 +17,7 @@ public class PlayerManager : MonoBehaviour {
 	
 	void Start () {
         //Debug.Log ("<color=blue>Test</color>");
+        Initialize();
     }
 
     void Update () {
@@ -26,6 +31,32 @@ public class PlayerManager : MonoBehaviour {
         rBody.AddForce (movement * speed);
     }
 
+    public void Initialize(){
+
+        lightChangeFactor = 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "House")
+        {
+            Debug.Log("entered");
+            isHome = true;
+            lightChangeFactor = -4;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "House")
+        {
+            Debug.Log("exit");
+            isHome = false;
+            lightChangeFactor = 1;
+        }
+
+    }
 
 
     //END
